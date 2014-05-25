8054ef4095d3e3f9a9175a9d26de071cf94e6126
pderichs <patderichs@gmail.com>
2014-05-25 11:16:31 +0200

    Using each_with_object instead of reduce

--- lib/time_frame/time_frame_uniter.rb
+++ lib/time_frame/time_frame_uniter.rb
@@ -9,14 +9,13 @@ class TimeFrame
 
     def unite
       frames = @sorted ? @time_frames : @time_frames.sort_by(&:min)
-      frames.reduce([]) do |result, frame|
+      frames.each_with_object([]) do |frame, result|
         last_frame = result.last
         if last_frame && last_frame.cover?(frame.min)
           result[-1] = TimeFrame.new(min: last_frame.min, max: frame.max)
         else
           result << frame
         end
-        result
       end
     end
   end
