2e870830f2b5cf7b7a12fd8e83ba1ee971b1fe80
pderichs <patrick.derichs@invision.de>
2014-12-30 11:49:58 +0100

    Simplified code

--- lib/time_frame/time_frame_handler.rb
+++ lib/time_frame/time_frame_handler.rb
@@ -3,17 +3,7 @@ class TimeFrame
   # time_frame classes when passed into a where-clause
   class Handler
     def call(column, time_frame)
-      Arel::Nodes::Between.new(
-        column,
-        Arel::Nodes::And.new(children_by(time_frame))
-      )
-    end
-
-    def children_by(time_frame)
-      [
-        Arel::Nodes.build_quoted(time_frame.min),
-        Arel::Nodes.build_quoted(time_frame.max)
-      ]
+      column.in(time_frame.min..time_frame.max)
     end
   end
 end
