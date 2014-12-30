e5a89bc4e17b5b3233258fc7a2434b846ee7e280
pderichs <patrick.derichs@invision.de>
2014-12-30 11:53:56 +0100

    Adding some specs

--- spec/time_frame_handler_spec.rb
+++ spec/time_frame_handler_spec.rb
@@ -3,12 +3,39 @@ require 'models/vogon_poem'
 
 describe TimeFrame::Handler do
   let(:time_frame) { TimeFrame.new(min: 5.days.ago, duration: 20.days) }
-  let!(:poem_min) { VogonPoem.create(written_at: time_frame.min) }
-  let!(:poem_max) { VogonPoem.create(written_at: time_frame.max) }
+
+  before { VogonPoem.delete_all }
 
   it 'returns all records between min and max' do
-    expect(VogonPoem.where(written_at: time_frame).count).to eq 2
-    expect(VogonPoem.where(written_at: time_frame).first).to eq poem_min
-    expect(VogonPoem.where(written_at: time_frame).last).to eq poem_max
+    poem_min = VogonPoem.create(written_at: time_frame.min)
+    poem_max = VogonPoem.create(written_at: time_frame.max)
+
+    result = VogonPoem.where(written_at: time_frame)
+
+    expect(result.count).to eq 2
+    expect(result.first).to eq poem_min
+    expect(result.last).to eq poem_max
+  end
+
+  it 'ignores records outside of min and max' do
+    poem_min = VogonPoem.create(written_at: time_frame.min + 1.minute)
+    VogonPoem.create(written_at: time_frame.min - 2.years)
+    VogonPoem.create(written_at: time_frame.max + 1.month)
+
+    result = VogonPoem.where(written_at: time_frame)
+
+    expect(result.count).to eq 1
+    expect(result.first).to eq poem_min
+  end
+
+  it 'handles items close to the borders correctly' do
+    poem_min = VogonPoem.create(written_at: time_frame.min + 1.second)
+    VogonPoem.create(written_at: time_frame.min - 1.second)
+    VogonPoem.create(written_at: time_frame.max + 1.second)
+
+    result = VogonPoem.where(written_at: time_frame)
+
+    expect(result.count).to eq 1
+    expect(result.first).to eq poem_min
   end
 end
